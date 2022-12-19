3ab34e9006609517d842803d308f7d01cf9acb56
Rob <robert.laubscher@gmail.com>
2022-12-19 07:12:47 +0200

    signup form work

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 
 def home(request):
     return render(request, 'todo/home.html')
 
 def signupuser(request):
-    return render(request, 'todo/signup.html',{'form':UserCreationForm})
+    if request.method == 'GET':
+        return render(request, 'todo/signup.html',{'form':UserCreationForm})
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            user = User.objects.create_user(request.POST['username'],password=request.POST['password1'])
+            user.save()
+        else:
+            print("Passwords didn't match")
