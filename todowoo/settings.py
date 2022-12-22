09bfc0f9f47e0d3fd00cf83c4d493790df9d0c64
Rob <robert.laubscher@gmail.com>
2022-12-22 21:55:27 +0200

    mysql settings added

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'todowoo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'kodahost_todowoo',
+        'USER': 'kodahost_django',
+        'PASSWORD': 'mRNSY5',
+        'HOST': '198.38.82.92',
+        #'PORT': '3306',
     }
 }
 
